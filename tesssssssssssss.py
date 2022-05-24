6e5e8595ca3c4d8876208a3bd38915a59fbf2379
Mukesh Y <mukesh.y@ivis.net>
2022-05-24 12:05:54 +0530

    gasss

---
+++ tesssssssssssss.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+hass=(datetime.today().strftime("%p"))
+a = ()
+if 
+def timetemp(lat,long,api_key):
+    return(hass,a)
+
+a = timetemp('27.2046° N','77.4977° E','704f97d5772e95806ef75145f4f87fd1')
+print(a)
